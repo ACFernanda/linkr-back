04036d1da69eee9b5c1e0ffeea6edc6106760545
acfernanda <acfernanda98@gmail.com>
2022-06-14 11:47:55 -0300

    Update tables file

---
+++ config/tables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+  "username" TEXT NOT NULL UNIQUE,
+	"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "sessions" (
+	"id" SERIAL PRIMARY KEY NOT NULL,
+	"token" TEXT NOT NULL UNIQUE,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+  "active" BOOLEAN NOT NULL DEFAULT 'true',
+	"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "posts" (
+	"id" SERIAL PRIMARY KEY NOT NULL,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+	"url" TEXT NOT NULL,
+	"description" TEXT,
+	"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "likes" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "postId" INTEGER NOT NULL REFERENCES "posts"("id"),
+  "userId" INTEGER NOT NULL REFERENCES "users"("id")
+);
+
+CREATE TABLE "hashtags" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE "post_hashtag" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "postId" INTEGER NOT NULL REFERENCES "posts"("id"),
+  "hashtagId" INTEGER NOT NULL REFERENCES "hashtags"("id")
+);
+
