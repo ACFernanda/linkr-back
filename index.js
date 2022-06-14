78b765a098ab7ef350390ca163c85314db56bdce
acfernanda <acfernanda98@gmail.com>
2022-06-14 11:53:25 -0300

    Update index.js with CORS

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express, { json } from "express";
+import cors from "cors";
+import chalk from "chalk";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,10 +9,11 @@ import router from "./routes/index.js";
 const app = express();
 
 app.use(json());
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Mode: ${process.env.MODE || "DEV"}`);
-  console.log(`Server is up on port: ${port}`);
+  console.log(chalk.bold.green(`Server is up on port: ${port}`));
 });
