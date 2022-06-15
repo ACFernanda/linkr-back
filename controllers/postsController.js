0e1968c8910ff8af6861ef2f138a8a0668140b0e
acfernanda <acfernanda98@gmail.com>
2022-06-15 12:12:06 -0300

    Update feat publish  new post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -18,26 +18,26 @@ export async function publishNewPost(req, res) {
   let urlTitle = "";
   let urlDescription = "";
   let urlImage = "";
+  let post = { userId, url, description };
 
   urlMetadata(url).then(
-    function (metadata) {
+    async function (metadata) {
       console.log(metadata);
       urlTitle = metadata.title;
       urlDescription = metadata.description;
       urlImage = metadata.image;
+      post = { ...post, urlTitle, urlDescription, urlImage };
+
+      try {
+        await postsRepository.insertNewPost(post);
+        res.sendStatus(201);
+      } catch (e) {
+        console.log(e);
+        res.status(500).send("Ocorreu um erro ao publicar post!");
+      }
     },
     function (error) {
       console.log(error);
     }
   );
-
-  const post = { userId, url, description, urlTitle, urlDescription, urlImage };
-
-  try {
-    await postsRepository.insertNewPost(post);
-    res.sendStatus(201);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Ocorreu um erro ao publicar post!");
-  }
 }
