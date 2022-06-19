885824f9ee17fe70945ba6946d9d35395d6156e2
acfernanda <acfernanda98@gmail.com>
2022-06-19 18:53:32 -0300

    Update feat likes: tooltip

--- repositories/likesRepository.js
+++ repositories/likesRepository.js
@@ -16,7 +16,19 @@ async function dislikePost(userId, postId) {
   );
 }
 
+async function getLikes(postId) {
+  return db.query(
+    `
+    SELECT users.id AS "userId", users.username
+    FROM likes
+    JOIN users ON likes."userId" = users.id
+    WHERE "postId"=$1`,
+    [postId]
+  );
+}
+
 export const likesRepository = {
   likePost,
   dislikePost,
+  getLikes,
 };
